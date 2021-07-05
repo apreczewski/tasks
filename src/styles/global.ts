3088d3ac37dabd276a8b9eb3a506b7fccba8b8d1
Alexandre Preczewski <apreczewski@gmail.com>
2021-07-05 12:52:49 -0300

    update project tasks, update responsive card and home page

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,26 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     overflow: hidden;
     margin: 0 10rem;
+
+    @media (max-width: 800px) {
+      margin: 0 5rem;
+    }
+
+    @media (max-width: 700px) {
+      margin: 0 4rem;
+    }
+
+    @media (max-width: 600px) {
+      margin: 0 3rem;
+    }
+
+    @media (max-width: 500px) {
+      margin: 0 2rem;
+    }
+
+    @media (max-width: 400px) {
+      margin: 0 1rem;
+    }
   }
 
   body, input, button, textarea {
